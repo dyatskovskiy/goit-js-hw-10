8e2b5db1ab89987fbfa3ee0f75d76375a0289ede
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-09-25 21:19:28 +0300

    catch error bugfix

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,13 +3,21 @@ axios.defaults.headers.common['x-api-key'] =
   'live_QSdbxLEb4GcI0swObeiCmN9WZHqJxUG5eKQXcDAOeqJ4lr1OQoJ7Krzuyo7F9tK7';
 
 export const fetchBreeds = function fetchBreeds() {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data);
+  return axios.get('https://api.thecatapi.com/v1/breeds').then(response => {
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  });
 };
 
 export const fetchCatByBreed = function fetchCatByBreed(breedId) {
   return axios
     .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => response.data);
+    .then(response => {
+      if (response.status < 200 || response.status > 299) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    });
 };
